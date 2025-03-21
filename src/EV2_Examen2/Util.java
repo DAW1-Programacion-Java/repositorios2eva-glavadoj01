06f6b7f1f7863c607205484ffd5a2c18520265ba
Gonzalo <gonzalolj@hotmail.es>
2025-03-21 15:59:50 +0100

    END 2º EV - Final Update

---
+++ src/EV2_Examen2/Util.java
@@ -0,0 +1,8 @@
+package EV2_Examen2;
+
+import java.time.format.DateTimeFormatter;
+
+public class Util {
+    public static final DateTimeFormatter FORMATO = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+}
---
+++ src/Tema4/ejercicio3/Tipo_Motor.java
@@ -0,0 +1,5 @@
+package Tema4.ejercicio3;
+
+public enum Tipo_Motor {
+    Diesel, Gasolina
+}
